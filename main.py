d062b7be9d92ef78992cbc0ae68b2d3502924045
KAPIL INANIA <67285213+kapilinania@users.noreply.github.com>
2023-07-19 20:59:55 +0530

    merge

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import PyPDF2
 
+# first module import
 pdfiles = ["1.pdf", "2.pdf"]
+# create pdf colletion
 merger = PyPDF2.PdfMerger()
 for filename in pdfiles:
-        pdfFile = open(filename, 'rb')
-        pdfReader = PyPDF2.PdfReader(pdfFile)
-        merger.append(pdfReader)
+    pdfFile = open(filename, 'rb')
+    pdfReader = PyPDF2.PdfReader(pdfFile)
+    merger.append(pdfReader)
 pdfFile.close()
 merger.write('merged.pdf')
+# final merge pdf name
