4cb08ea76a048f7634d471b6bc7cb0ba74bef81c
KAPIL INANIA <67285213+kapilinania@users.noreply.github.com>
2023-07-19 21:00:30 +0530

    merge final

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ merger = PyPDF2.PdfMerger()
 for filename in pdfiles:
     pdfFile = open(filename, 'rb')
     pdfReader = PyPDF2.PdfReader(pdfFile)
+    # here first pdf read and after append
     merger.append(pdfReader)
 pdfFile.close()
 merger.write('merged.pdf')
